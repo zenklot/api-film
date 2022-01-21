21fcc0ea97c4d9f0f6ee9e2d5a2f8e3f46f17dcf
zenklot <zenklot11@gmail.com>
2022-01-22 00:03:13 +0700

    init project & add router schema

---
+++ be-film/app/router.go
@@ -0,0 +1,16 @@
+package app
+
+import "github.com/julienschmidt/httprouter"
+
+func NewRouter(filmController controller.FilmController) *httprouter.Router {
+	router := httprouter.New()
+
+	router.POST("/api/film", filmController.Create)
+	router.GET("/api/films", filmController.FindAll)
+	router.GET("/api/films/search/:title", filmController.FindByTitle)
+	router.GET("/api/film/:filmId", filmController.FindById)
+	router.DELETE("/api/film/:filmId", filmController.Delete)
+
+	router.PanicHandler = exception.ErrorHandler
+	return router
+}
